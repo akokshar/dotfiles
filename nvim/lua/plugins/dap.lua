91680676bc7598e3fd5e1d58fa109c4371d5775e
lex <lex@fake.com>
2024-09-11 13:59:43 +0200

    updates, dap mappings

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -36,7 +36,7 @@ return {
 			vim.keymap.set("n", "<F7>", dap.step_out, {})
 			vim.keymap.set("n", "<F8>", dap.step_into, {})
 			vim.keymap.set("n", "<F9>", dap.step_over, {})
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<F4>", dap.toggle_breakpoint, {})
 		end,
 	},
 }
