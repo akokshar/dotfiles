1f1fc63be9bdea88551a60a49e1b56c1006f492e
lex <lex@fake.com>
2024-09-16 10:42:15 +0200

    add whichkey

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -9,14 +9,49 @@ return {
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
-      "leoluz/nvim-dap-go",
+			"leoluz/nvim-dap-go",
 		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
+			local dap_go = require("dap-go")
 
-      require("dap-go").setup()
-      require("dapui").setup()
+			dap_go.setup()
+			--require("dap-go").setup()
+			dapui.setup()
+			--require("dapui").setup()
+
+			dap.adapters.delve = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "dlv",
+					args = { "dap", "-l", "127.0.0.1:${port}" },
+					-- add this if on windows, otherwise server won't open successfully
+					-- detached = false
+				},
+				tests = {
+					-- enables verbosity when running the test.
+					verbose = true,
+				},
+			}
+
+			dap.configurations.go = {
+				{
+					type = "delve",
+					name = "Debug",
+					request = "launch",
+					program = "${file}",
+				},
+				{
+					type = "delve",
+					name = "Debug test", -- configuration for debugging test files
+					request = "launch",
+					mode = "test",
+					program = "${fileDirname}",
+					--buildFlags = "--test.v",
+				},
+			}
 
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
@@ -31,12 +66,13 @@ return {
 				dapui.close()
 			end
 
-			vim.keymap.set("n", "<F5>", dap.continue, {})
-			vim.keymap.set("n", "<F6>", dap.terminate, {})
-			vim.keymap.set("n", "<F7>", dap.step_out, {})
-			vim.keymap.set("n", "<F8>", dap.step_into, {})
-			vim.keymap.set("n", "<F9>", dap.step_over, {})
-			vim.keymap.set("n", "<F4>", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<F4>", dap.toggle_breakpoint, { desc = "Toggle breakpoing" })
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue" })
+			vim.keymap.set("n", "<F6>", dap_go.debug_test, { desc = "Debug test" })
+			vim.keymap.set("n", "<F7>", dap.step_out, { desc = "Step out" })
+			vim.keymap.set("n", "<F8>", dap.step_into, { desc = "Step into" })
+			vim.keymap.set("n", "<F9>", dap.step_over, { desc = "Step over" })
+			vim.keymap.set("n", "<F10>", dap.terminate, { desc = "Terminate" })
 		end,
 	},
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,149 +1,149 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
-      })
-    end,
-  },
-  --{
-  --	"ray-x/lsp_signature.nvim",
-  --	event = "VeryLazy",
-  --	config = function()
-  --		require("lsp_signature").setup({
-  --			bind = true, -- This is mandatory, otherwise border config won't get registered.
-  --			handler_opts = {
-  --				border = "rounded",
-  --			},
-  --			close_timeout = 100,
-  --			hint_enable = true,
-  --			floating_window = false,
-  --			hint_inline = function()
-  --				return "right_align"
-  --			end,
-  --		})
-  --	end,
-  --},
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.terraform_validate,
-          --null_ls.builtins.formatting.prettier,
-          --null_ls.builtins.diagnostics.erb_lint,
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        vim.lsp.protocol.make_client_capabilities(),
-        require("cmp_nvim_lsp").default_capabilities()
-      )
-      --capabilities.textDocument.completion.completionItem.snippetSupport = true
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+		end,
+	},
+	--{
+	--	"ray-x/lsp_signature.nvim",
+	--	event = "VeryLazy",
+	--	config = function()
+	--		require("lsp_signature").setup({
+	--			bind = true, -- This is mandatory, otherwise border config won't get registered.
+	--			handler_opts = {
+	--				border = "rounded",
+	--			},
+	--			close_timeout = 100,
+	--			hint_enable = true,
+	--			floating_window = false,
+	--			hint_inline = function()
+	--				return "right_align"
+	--			end,
+	--		})
+	--	end,
+	--},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.terraform_validate,
+					--null_ls.builtins.formatting.prettier,
+					--null_ls.builtins.diagnostics.erb_lint,
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				vim.lsp.protocol.make_client_capabilities(),
+				require("cmp_nvim_lsp").default_capabilities()
+			)
+			--capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      local on_attach = function(_, bufnr)
-        local function buf_set_option(...)
-          vim.api.nvim_buf_set_option(bufnr, ...)
-        end
-        buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+			local on_attach = function(_, bufnr)
+				local function buf_set_option(...)
+					vim.api.nvim_buf_set_option(bufnr, ...)
+				end
+				buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-        --vim.api.nvim_create_autocmd("CursorHold", {
-        --  buffer = bufnr,
-        --  callback = function()
-        --    local opts = {
-        --      focusable = false,
-        --      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-        --      border = "rounded",
-        --      source = "always",
-        --      prefix = " ",
-        --      scope = "cursor",
-        --    }
-        --    vim.diagnostic.open_float(nil, opts)
-        --  end,
-        --})
+				--vim.api.nvim_create_autocmd("CursorHold", {
+				--  buffer = bufnr,
+				--  callback = function()
+				--    local opts = {
+				--      focusable = false,
+				--      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				--      border = "rounded",
+				--      source = "always",
+				--      prefix = " ",
+				--      scope = "cursor",
+				--    }
+				--    vim.diagnostic.open_float(nil, opts)
+				--  end,
+				--})
 
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, opts)
-        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+				local opts = { buffer = bufnr, noremap = true, silent = true }
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("keep", { desc = "Code action" }, opts))
+				vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, vim.tbl_extend("keep", { desc = "Format code" }, opts))
+				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("keep", { desc = "Rename" }, opts))
 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("keep", { desc = "Hover" }, opts))
+				vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("keep", { desc = "Signature help" }, opts))
 
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        --vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-        --vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        --vim.keymap.set('n', '<space>wl', function()
-        --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        --end, opts)
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("keep", { desc = "Go to declaration" }, opts))
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("keep", { desc = "Go to definition" }, opts))
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("keep", { desc = "Go to implementation" }, opts))
+				--vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+				--vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+				--vim.keymap.set('n', '<space>wl', function()
+				--  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				--end, opts)
+				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, vim.tbl_extend("keep", { desc = "Type definition" }, opts))
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("keep", { desc = "References" }, opts))
 
-        --vim.keymap.set("n", "<leader>d", ":Telescope diagnostics<CR>", { desc = "Diagnostics in telescope" })
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-      end
+				--vim.keymap.set("n", "<leader>d", ":Telescope diagnostics<CR>", { desc = "Diagnostics in telescope" })
+				vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, vim.tbl_extend("keep", { desc = "Diagnostic" }, opts))
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("keep", { desc = "Diagnostic prev" }, opts))
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("keep", { desc = "Diagnostic next" }, opts))
+				vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, vim.tbl_extend("keep", { desc = "Diagnostic locations list" }, opts))
+			end
 
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        init_options = {
-          usePlaceholders = true,
-        },
-      })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				init_options = {
+					usePlaceholders = true,
+				},
+			})
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      lspconfig.terraformls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        init_options = {
-          experimentalFeatures = {
-            validateOnSave = false,
-            prefillRequiredFields = true,
-          },
-          validation = {
-            enableEnhancedValidation = true,
-          },
-        },
-      })
-    end,
-  },
+			lspconfig.terraformls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				init_options = {
+					experimentalFeatures = {
+						validateOnSave = false,
+						prefillRequiredFields = true,
+					},
+					validation = {
+						enableEnhancedValidation = true,
+					},
+				},
+			})
+		end,
+	},
 }
