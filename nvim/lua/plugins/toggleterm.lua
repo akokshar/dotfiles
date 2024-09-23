c4d3384fd52ff5e2635ceeeae2a19292df091624
lex <lex@fake.com>
2024-09-23 14:42:56 +0200

    feat(lazygit): add keymap

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,46 +1,50 @@
 return {
-	"akinsho/toggleterm.nvim",
-	name = "toggleterm",
-	priority = 1000,
-	config = function()
-		require("toggleterm").setup({
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 25
-				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.4
-				end
-			end,
+  "akinsho/toggleterm.nvim",
+  name = "toggleterm",
+  priority = 1000,
+  config = function()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 25
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
 
-			open_mapping = [[<c-\>]],
-			hide_numbers = true,
-			float_opts = {
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      float_opts = {
         border = "none",
-				width = function(_)
-					return vim.o.columns
-				end,
-				height = function(_)
-					return vim.o.lines - 1
-				end,
-			},
-			winbar = {
-				enabled = false,
-				name_formatter = function(term)
-					return term.name
-				end,
-			},
-		})
+        width = function(_)
+          return vim.o.columns
+        end,
+        height = function(_)
+          return vim.o.lines - 1
+        end,
+      },
+      winbar = {
+        enabled = false,
+        name_formatter = function(term)
+          return term.name
+        end,
+      },
+    })
 
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
       cmd = "lazygit",
       direction = "float",
       name = "lazygit",
-      hidden = true
+      hidden = true,
     })
 
-		function LazygitToggle()
-			lazygit:toggle()
-		end
-	end,
+    function LazygitToggle()
+      lazygit:toggle()
+    end
+
+    vim.keymap.set("n", "<leader>lg", function()
+      lazygit:toggle()
+    end, {})
+  end,
 }
