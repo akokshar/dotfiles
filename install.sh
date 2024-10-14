a00518774e25b7b7cf924e27a616ae244768c816
lex <lex@fake.com>
2024-10-14 12:38:59 +0200

    feat(treesitter): add textobjects

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# ln -s lazygit
+# ln -s tmux/tmux.conf
+# ln -s tmux/theme.conf
+#
+# git clone git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,63 @@
+-- https://www.josean.com/posts/nvim-treesitter-and-textobjects
+
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"go",
-					"hcl",
-					"terraform",
-					"lua",
-					"vim",
-					"javascript",
-					"html",
-					"yaml",
-				},
-				auto_install = true,
-				--ignore_install = { "dockerfile" },
-				highlight = {
-					enable = true,
-					--disable = { "dockerfile" },
-				},
-				indent = { enable = true },
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = true,
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        ensure_installed = {
+          "go",
+          "hcl",
+          "terraform",
+          "lua",
+          "vim",
+          "javascript",
+          "dockerfile",
+          "html",
+          "yaml",
+        },
+        --ignore_install = { "dockerfile" },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          --disable = { "dockerfile" },
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>v",
+            node_incremental = "<leader>v",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end,
+  },
 }
